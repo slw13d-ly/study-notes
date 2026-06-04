617348f53aa68b4c4ee1161048806da1722baf50
choi yujin <jinye24@naver.com>
2026-06-04 09:04:37 +0900

    변수

---
+++ java/src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,13 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+
+    public static void main(String[] args) {
+
+        // 변수 value 선언
+        int value;
+
+        // 연산 결과를 변수 result의 초기값으로 대입 
+    }
+    
+}
