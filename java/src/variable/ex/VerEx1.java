03a39a2d420d42ffc623146569586586cfe7f8c0
choi yujin <jinye24@naver.com>
2026-06-08 16:18:48 +0900

    변수 정리

---
+++ java/src/variable/ex/VerEx1.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VerEx1 {
+    public static void main(String[] args) {
+        // System.out.println(4 + 3);
+        // System.out.println(4 - 3);
+        // System.out.println(4 * 3);
+
+        int num1 = 4;
+        int num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
